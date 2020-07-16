7a3efcdff4abb2b998e2afa49f2fde7e5aef29f0
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-15 23:10:26 -0400

    {Monika] Model Class USer added with constructor and variables

--- app/src/main/java/com/c0767722/contact_monika_c0767722_android/model/User.java
+++ app/src/main/java/com/c0767722/contact_monika_c0767722_android/model/User.java
@@ -1,4 +1,141 @@
 package com.c0767722.contact_monika_c0767722_android.model;
 
-public class User {
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Entity(tableName = "user")
+public class User implements Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+
+    @SerializedName("lastName")
+    @Expose
+    private String lastName;
+
+    @SerializedName("email")
+    @Expose
+    private String email;
+    
+    @SerializedName("address")
+    @Expose
+    private String address;
+
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+
+    public User(String firstName, String lastName, String email, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    protected User(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readInt();
+        }
+        firstName = in.readString();
+        lastName = in.readString();
+        email = in.readString();
+        address = in.readString();
+        phone = in.readString();
+    }
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte( (byte) 0 );
+        } else {
+            dest.writeByte( (byte) 1 );
+            dest.writeInt( id );
+        }
+        dest.writeString(firstName );
+        dest.writeString(lastName);
+        dest.writeString(email);
+        dest.writeString(address);
+        dest.writeString(phone);
+
+    }
 }
