f47b7fe3eea4713ff3313c31571f72bfd2f92557
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-15 23:00:51 -0400

    {Monika]UserDb abd person class added

---
+++ app/src/main/java/com/c0767722/contact_monika_c0767722_android/database/UserDb.java
@@ -0,0 +1,4 @@
+package com.c0767722.contact_monika_c0767722_android.database;
+
+public class UserDb {
+}
---
+++ app/src/main/java/com/c0767722/contact_monika_c0767722_android/model/User.java
@@ -0,0 +1,4 @@
+package com.c0767722.contact_monika_c0767722_android.model;
+
+public class User {
+}
