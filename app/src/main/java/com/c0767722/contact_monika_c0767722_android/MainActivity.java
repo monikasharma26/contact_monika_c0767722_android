57bb5e776a269c048eecba1d046fde97b948414a
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-15 21:27:24 -0400

    Initial commit

---
+++ app/src/main/java/com/c0767722/contact_monika_c0767722_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.c0767722.contact_monika_c0767722_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
